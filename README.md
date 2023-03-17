632eacd199bd3c5a9008376a845b6394c45b3bf1
2910naveen <114217873+2910naveen@users.noreply.github.com>
2023-03-17 08:04:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# configToenv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-tek4yt)
---
+++ index.js
@@ -0,0 +1,81 @@
+const obj = {
+  naveen:{
+            name:"Segu.Naveen",
+            company:"Incedo",
+            techstack:{
+                         tech1:"java",
+                         tech2:"react.js",
+                         tech3:"node.js"
+                      },
+            education:{
+                           tenth:{
+                                   school:"RavindraBharathiSchool",
+                                   percentage:"95%"
+                                 },
+                           inter:{
+                                     college:"NarayanaJuniorCollege",
+                                     percentage:
+                                     "98.4%"
+                                  },
+                           btech:{
+                                     college:"SASTRA Deemend to be University",
+                                     cgpa:"8.34"
+                                 }
+                      }
+         },
+         sushith:{
+            name:"Sushith.Kalpam",
+            company:"Incedo",
+            techstack:{
+                         tech1:"java",
+                         tech2:"react.js",
+                         tech3:"node.js"
+                      },
+            education:{
+                           tenth:{
+                                   school:"RavindraBharathiSchool",
+                                   percentage:"95%"
+                                 },
+                           inter:{
+                                     college:"NarayanaJuniorCollege",
+                                     percentage:
+                                     "98.4%"
+                                  },
+                           btech:{
+                                     college:"SASTRA Deemend to be University",
+                                     cgpa:"8.34"
+                                 }
+                      }
+         }
+}
+
+
+function configToEnv(obj,str="")
+{
+    if(typeof obj !== "object")
+    {
+        return;
+    }
+    const objkeys = Object.keys(obj);
+    for(let i=0;i<objkeys.length;i++)
+    {
+       if(Object.keys(obj[objkeys[i]]).length>0)
+       {
+           if(str === "")
+           {
+              str = str + objkeys[i]; 
+           }
+           else
+           {
+               str = str+"_"+objkeys[i];
+           }
+           configToEnv(obj[objkeys[i]],str);
+       }
+       if(typeof obj[objkeys[i]] !== "object")
+       {
+        console.log(str+" "+obj[objkeys[i]]);   
+       }
+       str = str.substring(0,str.lastIndexOf('_'));
+    }
+}
+configToEnv(obj,"");
